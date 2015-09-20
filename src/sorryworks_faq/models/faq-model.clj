d04c0bee7e00f5d5cd1296e3ba505e0baaec5dbf
Todd Crone <todd.crone@perceptivesoftware.com>
2015-09-20 13:00:56 -0400

    Finally got first query to work

--- src/sorryworks_faq/models/faq-model.clj
+++ src/sorryworks_faq/models/faq-model.clj
@@ -1,2 +1,10 @@
 (ns sorryworks-faq.models.faq
-  (:require [yesql.core :refer [defqueries]])
+  (:require [yesql.core :refer [defqueries]]))
+
+(def db-spec {:classname  "org.postgresql.Driver"
+                :subprotocol  "postgresql"
+                :subname      "//localhost/sorryworks"})
+
+(defqueries "sorryworks_faq/models/faq.sql")
+
+(prn (get-faqs db-spec))
