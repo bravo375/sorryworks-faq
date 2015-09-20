347ba3599822c0516f5320dcd8fce057c115d0a0
Todd Crone <todd.crone@perceptivesoftware.com>
2015-09-20 13:23:04 -0400

    Format model file

--- src/sorryworks_faq/models/faq_model.clj
+++ src/sorryworks_faq/models/faq_model.clj
@@ -1,10 +1,8 @@
 (ns sorryworks-faq.models.faq-model
   (:require [yesql.core :refer [defqueries]]))
 
-(def db-spec {:classname  "org.postgresql.Driver"
-                :subprotocol  "postgresql"
-                :subname      "//localhost/sorryworks"})
+(def db-spec {:classname    "org.postgresql.Driver"
+              :subprotocol  "postgresql"
+              :subname      "//localhost/sorryworks"})
 
 (defqueries "sorryworks_faq/models/faq.sql")
-
-(prn (get-faqs db-spec))
