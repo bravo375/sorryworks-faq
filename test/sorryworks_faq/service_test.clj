3d16289c96b7bc3020370e7d2d8015998a3f687c
Todd Crone <todd.crone@perceptivesoftware.com>
2015-09-20 20:17:46 -0400

    Fix the tests

--- test/sorryworks_faq/service_test.clj
+++ test/sorryworks_faq/service_test.clj
@@ -9,31 +9,30 @@
 (deftest get-faqs-test
   (is (= (count (faq/get-faqs db-spec)) 3)))
 
-;; (def service
-;;   (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
+(def service
+  (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
 
-;; (deftest home-page-test
-;;   (is (=
-;;        (:body (response-for service :get "/"))
-;;        "Hello World!"))
-;;   (is (=
-;;        (:headers (response-for service :get "/"))
-;;        {"Content-Type" "text/html;charset=UTF-8"
-;;         "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-;;         "X-Frame-Options" "DENY"
-;;         "X-Content-Type-Options" "nosniff"
-;;         "X-XSS-Protection" "1; mode=block"})))
+(deftest home-page-test
+  (is (=
+       (:body (response-for service :get "/"))
+       "Sorry Works FAQ"))
+  (is (=
+       (:headers (response-for service :get "/"))
+       {"Content-Type" "text/html;charset=UTF-8"
+        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
+        "X-Frame-Options" "DENY"
+        "X-Content-Type-Options" "nosniff"
+        "X-XSS-Protection" "1; mode=block"})))
 
 
-;; (deftest about-page-test
-;;   (is (.contains
-;;        (:body (response-for service :get "/about"))
-;;        "Clojure 1.6"))
-;;   (is (=
-;;        (:headers (response-for service :get "/about"))
-;;        {"Content-Type" "text/html;charset=UTF-8"
-;;         "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-;;         "X-Frame-Options" "DENY"
-;;         "X-Content-Type-Options" "nosniff"
-;;         "X-XSS-Protection" "1; mode=block"})))
-
+(deftest about-page-test
+  (is (.contains
+       (:body (response-for service :get "/about"))
+       "Clojure 1.6"))
+  (is (=
+       (:headers (response-for service :get "/about"))
+       {"Content-Type" "text/html;charset=UTF-8"
+        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
+        "X-Frame-Options" "DENY"
+        "X-Content-Type-Options" "nosniff"
+        "X-XSS-Protection" "1; mode=block"})))
